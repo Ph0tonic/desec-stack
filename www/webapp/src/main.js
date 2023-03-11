26a8aae0cccd81620e82b845b62cf0dd7551adaf
Lukas Lihotzki <lukas@lihotzki.de>
2023-03-12 00:28:40 +0100

    refactor(webapp): switch to vue-timeago3

--- www/webapp/src/main.js
+++ www/webapp/src/main.js
@@ -12,6 +12,12 @@ import "@fontsource/roboto/400.css" /* regular */
 import "@fontsource/roboto/400-italic.css" /* regular-italic */
 import "@fontsource/roboto/500.css" /* medium */
 import "@fontsource/roboto/700.css" /* bold */
+<<<<<<< HEAD
+=======
+import '@mdi/font/css/materialdesignicons.css'
+
+import timeago from 'vue-timeago3'
+>>>>>>> 9326aca (refactor(webapp): switch to vue-timeago3)
 import { createPinia } from "pinia"
 
 import router from '@/router'
@@ -24,7 +30,15 @@ const vuetify = createVuetify({
 const pinia = createPinia()
 
 createApp(App)
+<<<<<<< HEAD
   .use(vuetify)
   .use(pinia)
   .use(router)
   .mount('#app')
+=======
+.use(vuetify)
+.use(timeago)
+.use(pinia)
+.use(router)
+.mount('#app')
+>>>>>>> 9326aca (refactor(webapp): switch to vue-timeago3)
